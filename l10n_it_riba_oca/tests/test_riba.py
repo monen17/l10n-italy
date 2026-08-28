c425c6d5a1d2ec58db55d7c79ffaa17b21cc723b
Simone Rubino <simone.rubino88@gmail.com>
2026-08-28 20:12:47 +0200

    [FIX] l10n_it_riba_oca: Allow to settle multiple lines
    Align `riba_line_settlement` to `16.0`

--- l10n_it_riba_oca/tests/test_riba.py
+++ l10n_it_riba_oca/tests/test_riba.py
@@ -1074,3 +1074,24 @@ class TestInvoiceDueCost(riba_common.TestRibaCommon):
             bank_fee_line.partner_id,
             "Bank fee line should not have partner_id when charge_to_customer is False",
         )
+
+    def test_button_settle_multiple_lines(self):
+        """
+        button_settle can handle multiple lines.
+        """
+        # Arrange
+        _invoice, riba_list = self.riba_sbf_common()
+        _invoice, other_riba_list = self.riba_sbf_common()
+        credited_lines = riba_list.line_ids[0] + other_riba_list.line_ids[0]
+
+        # Act
+        action = credited_lines.button_settle()
+
+        # Assert
+        self.assertEqual(action["res_model"], "riba.payment.multiple")
+        payment_wizard = (
+            self.env[action["res_model"]].with_context(**action["context"]).create({})
+        )
+        payment_wizard.pay()
+        self.assertEqual(riba_list.state, "paid")
+        self.assertEqual(other_riba_list.state, "paid")
